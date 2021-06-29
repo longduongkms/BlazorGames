4bff9008bbdf109415f7a589bee9e0c7c8a3681f
Long Duong <longduong@kms-technology.com>
2021-06-29 10:34:23 +0700

    minesweeper | Implement Minesweeper

---
+++ Games/Models/Minesweeper/Panel.cs
@@ -0,0 +1,34 @@
+﻿namespace Games.Models.Minesweeper
+{
+    public class Panel
+    {
+        public int Id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsMine { get; set; }
+        public int AdjacentMines { get; set; }
+        public bool IsRevealed { get; set; }
+        public bool IsFlagged { get; set; }
+
+        public Panel(int id, int x, int y)
+        {
+            Id = id;
+            X = x;
+            Y = y;
+        }
+
+        public void Flag()
+        {
+            if (!IsRevealed)
+            {
+                IsFlagged = !IsFlagged;
+            }
+        }
+
+        public void Reveal()
+        {
+            IsRevealed = true;
+            IsFlagged = false; //Revealed panels cannot be flagged
+        }
+    }
+}
